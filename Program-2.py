4d8eb21a94c8161d196137c3c00357c5b71a8e3d
BRIJESH SATA <brijesh.sata140587@marwadiuniversity.ac.in>
2026-02-16 15:17:28 +0530

    Delete Program-2.py

--- Program-2.py
+++
@@ -1,6 +0,0 @@
-#WAP for print sum of n natural numbers.
-n=int(input("Enter the Natural Number:="))
-total=0
-for i in range(1,n+1):
-    total =total+i**2
-print("Sum of First",n,"Natural Number is:",total)
